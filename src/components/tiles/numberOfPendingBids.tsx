eb522c0d00cb221413908733e239e2a10625a78e
alexdev2001 <aleximani201@gmail.com>
2025-02-28 16:02:12 +0200

    can now render number of active bids tile on home page

--- src/components/tiles/numberOfPendingBids.tsx
+++ src/components/tiles/numberOfPendingBids.tsx
@@ -1,10 +1,34 @@
+import {useEffect, useState} from "react";
+
 export default function NumberOfBids() {
+    const [numberOfActiveBids, setNumberOfActiveBids] = useState(0);
+
+    useEffect(() => {
+        const fetchBids = async () => {
+            try {
+                const response = await fetch('http://localhost:3002/bids');
+                if (!response.ok) {
+                    console.error('Bid not found.');
+                    return;
+                }
+
+                const data = await response.json();
+                const filteredData = data.filter((bid: any) => bid.status === true)
+                setNumberOfActiveBids(filteredData.length);
+            } catch (e) {
+                console.error(e);
+            }
+
+        }
+
+        fetchBids();
+    }, [])
 
     return (
         <div className="bg-white shadow-lg rounded-lg p-6 flex flex-col justify-center  w-1/3 h-64 font-custom">
             <div className="flex-row">
                 <p className="text-6xl font-extrabold text-blue-500 mt-10">
-                    2
+                    {numberOfActiveBids}
                 </p>
                 <div className="h-3"/>
             </div>
